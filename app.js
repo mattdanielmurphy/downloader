9bddb6d157b485fa79c6be41f0ab0948bb67293c
Matthew Murphy <matt@mattmurphy.ca>
2019-03-13 22:22:03 -0400

    make welcome message ASCII art

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ const prompts = require('prompts')
 
 const { Series } = require('./components/Series')
 const { Movie } = require('./components/Movie')
+const welcomeMessage = require('./components/Welcome-Message.js')
 
 async function welcome() {
-	console.log('Welcome to Pirateer!')
+	welcomeMessage()
 	const typeOfDownload = await prompts({
 		type: 'select',
 		name: 'typeOfDownload',
-		message: 'What would you like to download?',
-		choices: [ { title: 'Single File', value: 'movie' }, { title: 'TV Series', value: 'series' } ],
+		message: 'Ahoy! What would you like to download?',
+		choices: [ { title: 'Movie', value: 'movie' }, { title: 'TV Series', value: 'series' } ],
 		initial: 0
 	})
 	return typeOfDownload
 }
 
-function startApp() {
-	welcome().then(({ typeOfDownload }) => (typeOfDownload === 'movie' ? new Movie() : new Series()))
-}
+const startApp = () => welcome().then(({ typeOfDownload }) => (typeOfDownload === 'movie' ? new Movie() : new Series()))
 
 startApp()
